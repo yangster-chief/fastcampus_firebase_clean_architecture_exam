2034ba526addcd07e25429a211ed409b211fdf9e
thunder-ramgee <38216680+yangster-chief@users.noreply.github.com>
2024-02-20 00:42:33 +0900

    init

---
+++ android/app/src/main/kotlin/yangsterchif/fastcampus/exam/clean/fastcampus_firebase_clean_architecture_exam/MainActivity.kt
@@ -0,0 +1,6 @@
+package yangsterchif.fastcampus.exam.clean.fastcampus_firebase_clean_architecture_exam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
